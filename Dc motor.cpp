adf26584c8b8b14350dd603a3719de8c36d8fdf1
TheNameisNavin <navinkumarspm@outlook.com>
2025-03-31 09:28:26 +0530

    Update Dc motor.cpp

--- Dc motor.cpp
+++ Dc motor.cpp
@@ -1,1 +1,18 @@
+void setup()
+{
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(8, HIGH);
+  delay(2000); // Wait for 2000 millisecond(s)
+  digitalWrite(8, LOW);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(7, HIGH);
+  delay(5000); // Wait for 5000 millisecond(s)
+  digitalWrite(7, LOW);
+  delay(5000); // Wait for 5000 millisecond(s)
+}
 
