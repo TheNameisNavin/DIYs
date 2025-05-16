72f544c6df7a59376574aa8a99ad3a752ee7603c
TheNameisNavin <navinkumarspm@outlook.com>
2025-05-16 19:10:04 +0530

    Create output_formatting_types.py

---
+++ output_formatting_types.py
@@ -0,0 +1,23 @@
+
+#usinf string modulo operator
+I/P:
+x=10
+y=20
+print("the values are %d%d" % (x,y))
+
+O/P:
+
+the values are 1020
+
+
+#using dot format method
+
+Syntax : print("{}{}....".format(variable1, variable2,..))
+
+I/P:
+
+x = 10
+y = 20
+print(f'the values are {x}{y})
+
+O/P: the values are 1020
