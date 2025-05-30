5bfe112f35cf1db082f3d909b41a60d0221071d3
TheNameisNavin <navinkumarspm@outlook.com>
2025-05-30 18:28:33 +0530

    Update et.py

--- et.py
+++ et.py
@@ -334,5 +334,101 @@ for _ in range(t):
         print(y-x)
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+'''
+Reach on Time
+Chef has recently moved into an apartment. It takes 
+30
+30 minutes for Chef to reach office from the apartment.
 
+Chef left for the office 
+X
+X minutes before Chef was supposed to reach. Determine whether or not Chef will be able to reach on time.
 
+Input Format
+The first line of input will contain a single integer 
+T
+T, denoting the number of test cases.
+Each test case consists of a single integer 
+X
+X.
+Output Format
+For each test case, output YES if Chef will reach on time, NO otherwise.
+
+The output is case-insensitive. Thus, the strings YES, yes, yeS, and Yes are all considered the same.
+
+Constraints
+1
+≤
+T
+≤
+60
+1≤T≤60
+1
+≤
+X
+≤
+60
+1≤X≤60
+'''
+
+Code:
+
+# cook your dish here
+t = int(input())
+for _ in range(t):
+    x = int(input())
+    if x >= 30 :
+        print("YES")
+    else:
+        print("NO")
+
+--------------------------------------------------------------------
+'''
+Maximum Submissions
+A participant can make 
+1
+1 submission every 
+30
+30 seconds. If a contest lasts for 
+X
+X minutes, what is the maximum number of submissions that the participant can make during it?
+
+It is also given that the participant cannot make any submission in the last 
+5
+5 seconds of the contest.
+
+Input Format
+The first line of input will contain a single integer 
+T
+T, denoting the number of test cases.
+Each test case consists of a single integer 
+X
+X, denoting the number of minutes.
+Output Format
+For each test case, output the maximum number of submissions a participant can make in 
+X
+X minutes.
+
+Constraints
+1
+≤
+T
+≤
+30
+1≤T≤30
+1
+≤
+X
+≤
+30
+1≤X≤30
+'''
+
+Code:
+
+# cook your dish here
+t = int(input())
+for _ in range(t):
+    x= int(input())
+    y = 2*x
+    print(y)
